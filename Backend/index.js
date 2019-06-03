b5af97670d08a847b58ac446ca5b4b36d44deeb1
Ravikiran303 <ravikiran2221@gmail.com>
2019-06-03 15:58:46 +0530

    Add Product post API

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,16 @@ const port = 5000;
 var mongoose = require("mongoose");
 const mongoURI = "mongodb://localhost:27017/SimpleApp";
 
+const Product = require("./models/Product");
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
 const cors = require("cors");
 app.use(cors());
 
@@ -14,7 +24,12 @@ mongoose
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Products");
 });
-
+app.post("/items", (req, res) => {
+  console.log(req.body);
+  Product.create(req.body).then(responce => {
+    res.status(201).json(responce);
+  });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
