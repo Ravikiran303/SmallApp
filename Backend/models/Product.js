d7363f6caf81374844c2111066c836cc08f40fb0
Ravikiran303 <ravikiran2221@gmail.com>
2019-06-03 21:08:24 +0530

    Change quantity to number

--- Backend/models/Product.js
+++ Backend/models/Product.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const Product_Schema = new Schema({
   title: String,
   price: String,
-  quantity: String,
+  quantity: Number,
   image: String
 });
 module.exports = Product = mongoose.model("Product", Product_Schema, "Product");
