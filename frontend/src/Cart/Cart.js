2a7d299e002765adc189984ffee56db508855292
Ravikiran303 <ravikiran2221@gmail.com>
2019-06-04 11:23:57 +0530

    Show cart items

--- frontend/src/Cart/Cart.js
+++ frontend/src/Cart/Cart.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
+import Axios from "axios";
 
 export class Cart extends Component {
+  state = { products: [] };
+  componentDidMount() {
+    Axios.get("/cart").then(res => {
+      this.setState({ products: res.data });
+      console.log(res.data);
+    });
+  }
   render() {
-    return <div>cart</div>;
+    return (
+      <div>
+        {this.state.products.map((items, index) => {
+          return (
+            <div className="item" key={index} id={index}>
+              <div>
+                <img className="image" src={items.image} alt="" />
+              </div>
+              <div className="details">
+                {items.title}
+                <br />
+                No of Items : {items.quantity}
+                <br />
+                Price : {items.quantity * items.price}
+                <br />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
